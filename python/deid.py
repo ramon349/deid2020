49a631458f85c66f3d92348f8676b8e40f7cd549
Ramon Correa <ramoncorrea@Ramons-MacBook-Pro.local>
2020-10-07 01:10:37 -0400

    draft implementation of age module

--- python/deid.py
+++ python/deid.py
@@ -1,12 +1,49 @@
 import re
 import sys
-phone_pattern ='(\d{3}[-\.\s/]??\d{3}[-\.\s/]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s/]??\d{4})'
-
+import numpy as np 
+#phone_pattern ='(\d{3}[-\.\s/]??\d{3}[-\.\s/]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s/]??\d{4})'
+age_pattern = r'\b\d\d\b'
 # compiling the reg_ex would save sime time!
-ph_reg = re.compile(phone_pattern)
+age_reg  = re.compile(age_pattern)
+age_hint= r'(year|old|yo)'
+age_hint_reg = re.compile(age_hint,flags=re.IGNORECASE)
+from nltk.tokenize import sent_tokenize 
 
 
-def check_for_phone(patient,note,chunk, output_handle):
+def is_close(age_candi,age_hint): 
+    age_end = age_candi.span()[1]
+    hint_start = age_hint.span()[1]
+    if  np.abs(age_end - hint_start) <= 6: 
+        return True
+    else: 
+        return False
+
+
+## our goal here would be to tokenize words and see the inclusion of certain key words
+def search_lines(chunk): 
+    #other_chunk = re.sub('START_OF_RECORD=\d\d?\|\|\|\|\d\d?\|\|\|\|\\nO:',"",chunk)
+    #chunk = other_chunk.lstrip()
+    #chunk = chunk.replace("\n","")
+    outputs = list()  
+    match = list(age_reg.finditer(chunk) )
+    exist = list(age_hint_reg.finditer(chunk) ) 
+    offset = 29
+    #breakpoint() 
+    if match and len(exist) >0 : 
+        for k in match:  
+            closeness = [is_close(k,w) for w in exist ]
+            if not np.any(closeness) :
+                continue
+            a,b = k.span() 
+            start=  k.start() -  offset 
+            end =  k.end()  - offset
+            result = str(start) + ' ' + str(start)  +' '+ str(end) 
+            #print(result)
+            #print(k)
+            outputs.append(result)
+    print('---end---')
+    return outputs
+def check_for_age(patient,note,chunk, output_handle):
     """
     Inputs:
         - patient: Patient Number, will be printed in each occurance of personal information found
@@ -24,7 +61,7 @@ def check_for_phone(patient,note,chunk, output_handle):
     """
     # The perl code handles texts a bit differently, 
     # we found that adding this offset to start and end positions would produce the same results
-    offset = 27
+    offset = 31
 
     # For each new note, the first line should be Patient X Note Y and then all the personal information positions
     output_handle.write('Patient {}\tNote {}\n'.format(patient,note))
@@ -33,18 +70,19 @@ def check_for_phone(patient,note,chunk, output_handle):
     # for each one write the results: "Start Start END"
     # Also for debugging purposes display on the screen (and don't write to file) 
     # the start, end and the actual personal information that we found
-    for match in ph_reg.finditer(chunk):
-                
-            # debug print, 'end=" "' stops print() from adding a new line
+    outputs = search_lines(chunk) 
+    for e in outputs: 
+        output_handle.write(e+'\n')
+    """
+    for match in age_reg.finditer(chunk):
+            # debug print, 'end=" "' stops print() from adding a new line 
             print(patient, note,end=' ')
             print((match.start()-offset),match.end()-offset, match.group())
-                
             # create the string that we want to write to file ('start start end')    
             result = str(match.start()-offset) + ' ' + str(match.start()-offset) +' '+ str(match.end()-offset) 
-            
             # write the result to one line of output
             output_handle.write(result+'\n')
-
+    """ 
             
             
 def deid_phone(text_path= 'id.text', output_path = 'phone.phi'):
@@ -76,10 +114,10 @@ def deid_phone(text_path= 'id.text', output_path = 'phone.phi'):
 
     # end of each note has the patter: ||||END_OF_RECORD
     end_of_record_pattern = '\|\|\|\|END_OF_RECORD$'
-
+    counter =1 
     # open the output file just once to save time on the time intensive IO
     with open(output_path,'w+') as output_file:
-        with open(text_path) as text:
+        with open(text_path,'r') as text:
             # initilize an empty chunk. Go through the input file line by line
             # whenever we see the start_of_record pattern, note patient and note numbers and start 
             # adding everything to the 'chunk' until we see the end_of_record.
@@ -93,10 +131,15 @@ def deid_phone(text_path= 'id.text', output_path = 'phone.phi'):
                 # check to see if we have seen the end of one note
                 record_end = re.findall(end_of_record_pattern, line,flags=re.IGNORECASE)
 
-                if len(record_end):
+                if len(record_end) :
                     # Now we have a full patient note stored in `chunk`, along with patient numerb and note number
                     # pass all to check_for_phone to find any phone numbers in note.
-                    check_for_phone(patient,note,chunk.strip(), output_file)
+                    #if int(patient) == 153:
+                    #   breakpoint()  
+                    print(f"----Start patient: {patient}----")
+                    check_for_age(patient,note,chunk.strip(), output_file)
+                    counter = counter + 1  
+                    record_end =[] 
                     
                     # initialize the chunk for the next note to be read
                     chunk = ''
