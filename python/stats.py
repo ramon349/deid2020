29acbaaad3b8c2e5387b4f52cc6243df7fdddb93
Ramon Correa <ramoncorrea@Ramons-MacBook-Pro.local>
2020-10-07 15:22:28 -0400

    stats.py 	changed the default parameter for run_stats funciton to look for the age.phi output file. Which should be the correct phi file name
    stats.txt
    	example output

--- python/stats.py
+++ python/stats.py
@@ -15,7 +15,7 @@ def are_overlapped(gstart,gend,tstart,tend):
 import re,sys
 from collections import defaultdict
 
-def run_stats(gold_path = 'id.deid', gold_cats_path= 'id-phi.phrase', test_path='phone.phi'):
+def run_stats(gold_path = 'id.deid', gold_cats_path= 'id-phi.phrase', test_path='age.phi'):
     """
     Inputs:
         gold_path: path to the gold standard file that does not include categories.
